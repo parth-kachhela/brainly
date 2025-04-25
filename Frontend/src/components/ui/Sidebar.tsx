bcddca34c02f021d653c45c3ea0bced22fcfce9e
Parth Kachhela <parthkachhela78@gmail.com>
2025-04-25 17:07:00 +0530

    init

--- Frontend/src/components/ui/Sidebar.tsx
+++ Frontend/src/components/ui/Sidebar.tsx
@@ -1,3 +1,4 @@
+import { LogoIcon } from "../Icons/LogoIcon";
 import { TwitterIcon } from "../Icons/TwitterIcon";
 import { YoutubeIcon } from "../Icons/YoutubeIcon";
 import { SidebarItem } from "./SidebarItem";
@@ -5,6 +6,12 @@ import { SidebarItem } from "./SidebarItem";
 export const Sidebar = () => {
   return (
     <div className="h-screen bg-white border-r w-76 fixed left-0 top-0 ">
+      <div className="flex text-2xl pt-4 items-center">
+        <div className="pr-2 text-purple-400">
+          <LogoIcon />
+        </div>
+        Brainly
+      </div>
       <SidebarItem text="Twitter" icon={<TwitterIcon />} />
       <SidebarItem text="Youtube" icon={<YoutubeIcon />} />
     </div>
