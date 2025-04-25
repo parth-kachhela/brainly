998bf6935a137ad07ad8492e3d39cc91f43fcacd
Parth Kachhela <parthkachhela78@gmail.com>
2025-04-25 12:58:09 +0530

    init

--- Frontend/src/components/ui/Sidebar.tsx
+++ Frontend/src/components/ui/Sidebar.tsx
@@ -1,3 +1,7 @@
 export const Sidebar = () => {
-  return <div>hye thre</div>;
+  return (
+    <div className="h-screen bg-white border-r w-76 fixed left-0 top-o ">
+      hye thre
+    </div>
+  );
 };
