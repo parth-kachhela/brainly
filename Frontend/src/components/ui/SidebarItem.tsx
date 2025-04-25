d58b9bc214d833b8d7c89a18cab8e22640a47091
Parth Kachhela <parthkachhela78@gmail.com>
2025-04-25 16:22:19 +0530

    init

---
+++ Frontend/src/components/ui/SidebarItem.tsx
@@ -0,0 +1,15 @@
+import { ReactElement } from "react";
+
+export function SidebarItem({
+  text,
+  icon,
+}: {
+  text: string;
+  icon: ReactElement;
+}) {
+  return (
+    <div className="flex">
+      {icon} {text}
+    </div>
+  );
+}
