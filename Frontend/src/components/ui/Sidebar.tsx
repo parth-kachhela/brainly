412333f191075e6ef8eb500d8cad30722a033a24
Parth Kachhela <parthkachhela78@gmail.com>
2025-04-25 16:38:55 +0530

    init

--- Frontend/src/components/ui/Sidebar.tsx
+++ Frontend/src/components/ui/Sidebar.tsx
@@ -1,10 +1,12 @@
 import { TwitterIcon } from "../Icons/TwitterIcon";
+import { YoutubeIcon } from "../Icons/YoutubeIcon";
 import { SidebarItem } from "./SidebarItem";
 
 export const Sidebar = () => {
   return (
     <div className="h-screen bg-white border-r w-76 fixed left-0 top-0 ">
       <SidebarItem text="Twitter" icon={<TwitterIcon />} />
+      <SidebarItem text="Youtube" icon={<YoutubeIcon />} />
     </div>
   );
 };
